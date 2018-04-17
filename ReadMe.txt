7a66583401c82c0f4b14d5d348e64ffe7375f355
zayan-nadeem <zayan.nadeem93@gmail.com>
2018-04-17 15:34:30 -0400

    comments added

---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+This is basically an Angular Application with LESS/CSS to sytle the front.
+
+To run this you would have to fetch it to a local folder.
+
+and run the httpserver
+
+I am adding screen shots in case as well.
+
+
+This application is only working for Chrome Right now. I did not have the chance to test it on FireFox/IE 
--- app/app.js
+++ app/app.js
@@ -1,10 +1,17 @@
 angular
-	.module('groceryApp',[])
+	.module('groceryApp',[])  //Main Module defined for the Angular Application
 	.controller('groceryCtrl', function($scope, $http) {
 
 		$scope.groceryList = new Array();
 		$scope.images = new Array();
 
+		/*
+		It is suggested to make the REST calls in Factories/Services to make the application
+		Singleton. Since, this is fairly simple, I have called the GET method here in the controller.
+		You can view other applications for the proper structure in my repository
+
+		This application is also robust since the categories aren't forced populated
+		*/
 		$http.get('data/images.json').then(function(res){ 
 			console.log(res.data)
 			$scope.images = res.data;
@@ -15,18 +22,14 @@ angular
 			addImages();
 		});
 
-		
+		/*AddImages updates the Groceries.JSON for this application so that the images are added within the application
+		No changes are made to the data files */
 		function addImages() {
 		$scope.groceryList.forEach(function(item) {
 			var x = item["category"];
-			console.log(x);
-			//console.log(Object.keys($scope.categories));
 			var link = $scope.images[x];
-			console.log(link);
 			item["category"] = link;
-			console.log(item["category"]);
-			console.log("----");
 		});
 	}
 
-	});
+});
